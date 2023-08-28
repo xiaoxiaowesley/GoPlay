2cba0a1ef08c88e7e5a3d1c9cc2636d9b52e355e
“seanxiaoxiang” <xx266218@alibaba-inc.com>
2023-08-29 00:47:12 +0800

    :sparkles: Search files and show it

--- GoPlay/GoPlay/Modules/SwiftUI/Main/AppFolderView.swift
+++ GoPlay/GoPlay/Modules/SwiftUI/Main/AppFolderView.swift
@@ -8,17 +8,43 @@
 
 import SwiftUI
 
+struct DataObject  {
+    let filename: String
+    let fullpath: String
+}
+
+extension DataObject: Hashable {
+    static func == (lhs: DataObject, rhs: DataObject) -> Bool {
+        return lhs.fullpath == rhs.fullpath && lhs.filename == rhs.filename
+    }
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(fullpath)
+        hasher.combine(filename)
+    }
+}
+
+
 struct AppFolderView: View {
     let title:String
-    let input: [String]
     let buttonAction: () -> Void // Expose the button's click action in the constructor
     
     @State private var showView = false
     @State private var rotationAngle: Double = 0
+    @State private var isAnimating = true // Add a state variable to control the animation
+    @State private var isLoading = false
+    @State private var input: [DataObject]
     
+    init(title: String, buttonAction: @escaping () -> Void) {
+        self.title = title
+        self.buttonAction = buttonAction
+        self.input = [] // Initialize the input array
+    }
     
     var body: some View {
-        Button(action: buttonAction) { // Use the exposed button action
+        Button(action: {
+            buttonAction();
+            isAnimating.toggle() // Toggle the animation state when the button is clicked
+        }) { // Use the exposed button action
             HStack(alignment: .top) {
                 Image(systemName: "externaldrive.fill")
                     .renderingMode(.template)
@@ -29,20 +55,47 @@ struct AppFolderView: View {
                     .renderingMode(.template)
                     .foregroundColor(.blue)
                     .rotationEffect(Angle(degrees: rotationAngle))
+                    .onAppear {
+                        withAnimation(Animation.linear(duration: 1).repeatForever(autoreverses: false)) {
+                            rotationAngle = 360
+                        }
+                    }
+                    .rotationEffect(Angle(degrees: isAnimating ? rotationAngle : 0)) // Apply rotation effect based on the animation state
+                
             }
         }
         .frame(minWidth: 0, maxWidth: .infinity, minHeight: 40, maxHeight: 40, alignment: .leading)
         .padding(.vertical, 2)
-        
-        
-        // foreach input to generate multi row
-        ForEach(input, id: \.self) { item in
-            Text(item).frame(height:30, alignment: .center)
-        }
-        
         .onAppear {
-            //            isAnimating = true // Start the animation when the view appears
+            print("onAppear")
+            
+            isLoading = true
+            DispatchQueue.global(qos: .background).async {
+                self.input = fetchVideoFiles()
+                DispatchQueue.main.async {
+                    isLoading = false
+                }
+            }
+            
         }
+        ForEach(input, id: \.self) { item in
+            Text(item.filename)
+        }
+    }
+    
+    func fetchVideoFiles() -> [DataObject] {
+        let fileManager = FileManager.default
+        let documentsURL = fileManager.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let docPath = documentsURL
+        do {
+            let fileURLs = try fileManager.contentsOfDirectory(at: docPath, includingPropertiesForKeys: nil)
+            let filePaths:[String]  = fileURLs.filter { $0.pathExtension == "mp4" }.map { $0.path }
+            let dataObjects = filePaths.map { DataObject(filename:URL(fileURLWithPath: $0).lastPathComponent, fullpath:  $0) }
+            return dataObjects
+        } catch {
+            print("Error while enumerating files \(docPath.path): \(error.localizedDescription)")
+        }
+        return []
     }
 }
 //
--- GoPlay/GoPlay/Modules/SwiftUI/Main/FolderTypeList.swift
+++ GoPlay/GoPlay/Modules/SwiftUI/Main/FolderTypeList.swift
@@ -8,22 +8,29 @@
 
 import SwiftUI
 
+
+
+
 struct FolderTypeList: View {
     @EnvironmentObject var hostingProvider: ViewControllerProvider
     
     
     var body: some View {
         List {
-            AppFolderView(title:NSLocalizedString("ApplicationFolder", comment: "The sandbox document folder"),input: ["String.txt","hello.mp4","hello.mp4","hello.mp4"],buttonAction: {
+          
+            AppFolderView(title: NSLocalizedString("ApplicationFolder", comment: "The sandbox document folder")) {
                 
-            })
+            }
             DocumentFolderView(buttonAction: {
                 FilesManager.shared.openFiles(viewController: hostingProvider.viewController!, action: {( urls: [URL]) -> Void in
                     
                 })
             }, title: NSLocalizedString("FilesFolder", comment: "iOS system Files application"))
+        }.onAppear(){
+            
         }
     }
+    
 }
 //
 //struct FolderTypeList_Previews: PreviewProvider {

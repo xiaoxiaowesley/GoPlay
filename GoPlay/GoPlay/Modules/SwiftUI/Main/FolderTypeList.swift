d30df44c077b80438da9ad1aebc97cfc5d8abf40
xiaoxiang <xx266218@alibaba-inc.com>
2023-08-28 20:12:06 +0800

    :globe_with_meridians: LocalizedString

--- GoPlay/GoPlay/Modules/SwiftUI/Main/FolderTypeList.swift
+++ GoPlay/GoPlay/Modules/SwiftUI/Main/FolderTypeList.swift
@@ -14,14 +14,14 @@ struct FolderTypeList: View {
     
     var body: some View {
         List {
-            AppFolderView(title:"应用文件夹",input: ["String.txt","hello.mp4","hello.mp4","hello.mp4"],buttonAction: {
+            AppFolderView(title:NSLocalizedString("ApplicationFolder", comment: "The sandbox document folder"),input: ["String.txt","hello.mp4","hello.mp4","hello.mp4"],buttonAction: {
                 
             })
             DocumentFolderView(buttonAction: {
-                FilesManager.shared.openFiles(viewController: hostingProvider.viewController!, action: {([URL]) -> Void in
-                    //print urls
+                FilesManager.shared.openFiles(viewController: hostingProvider.viewController!, action: {( urls: [URL]) -> Void in
+                    
                 })
-            }, title: "文件")
+            }, title: NSLocalizedString("FilesFolder", comment: "iOS system Files application"))
         }
     }
 }
